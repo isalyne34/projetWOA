b1dc8decd7c7a19d919c4df94068fe9adcf5671d
Isalyne <isalyne.llinares-rames@etu.umontpellier.fr>
2024-01-19 08:50:28 +0100

    fix : pageAide

--- frontend/src/pages/pageAide.tsx
+++ frontend/src/pages/pageAide.tsx
@@ -1,18 +1,36 @@
 //page d'aide
 import { ReactNode } from 'react';
 import Layout from '../layout/layout';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { useEffect } from 'react';
+
+
+
 
 export default function Aide() {
+   //aide
 
-    //aide
+   return (
+      <Layout>
+         <h1> Guide d'installations </h1> <br />
 
+         <p> !  Rafraichir la page afin d'afficher les modifications </p>
 
-    return (
-        <Layout>
-        <div>Aide </div>
-        </Layout>
-    );
+         <h3> Si la base de données est vide, vous pouvez :</h3>
+         <p>
+            {' '}
+            - Créer un voyage <br />
+            - Créer un utilisateur et l'ajouter au voyage <br />- Cliquer sur le voyage, et créer des dépenses en associant la dépense à la personne qui l'a créée
+         </p>
+         <p> ! Il faut créer l'utilisateur avant de pouvoir l'associer</p>  <br />
+
+         <h3> Si la base de données existe déjà, vous pouvez :</h3>
+         <p>
+            {' '}
+            - Modifier un voyage grâce au bouton crayon situé à droite <br />
+            - Supprimer un voyage <br />
+            - Rajouter un membre ou une dépense <br />
+            - Modifier une dépense <br />
+            - Modifier un utilisateur <br />- Voir le partage des dépenses{' '}
+         </p>
+      </Layout>
+   );
 }
